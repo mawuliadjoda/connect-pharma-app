51abe9fdaddad5c23c500bd2104b63a91fe158a4
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2023-11-26 13:03:54 +0100

    clean code

---
+++ connect-pharma/src/pages/auth/Login/FirebaseErrorCodeEnum.ts
@@ -0,0 +1,8 @@
+
+export enum FirebaseErrorCode {
+    USER_NOT_FOUND = 'auth/user-not-found',
+}
+
+export enum FirebaseErrorMessage {
+    USER_NOT_FOUND = 'V\u00E9rifier votre saisie ! utilisateur et mot de passe invalides',
+}
--- connect-pharma/src/utils/Utils.ts
+++ connect-pharma/src/utils/Utils.ts
@@ -19,11 +19,3 @@ export const buildEmail = (tel: string) => `${formatPhoneNumber(tel).trim()}@${i
 
 
 export const trimString = (value: string) => value ? value.trim() : value;
-
-export enum FirebaseErrorCode {
-    USER_NOT_FOUND = 'auth/user-not-found',
-}
-
-export enum FirebaseErrorMessage {
-    USER_NOT_FOUND = 'V\u00E9rifier votre saisie ! utilisateur et mot de passe invalides',
-}
